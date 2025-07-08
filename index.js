121a3e88d6a22d16651566a310149e4e0b3ac058
sadiyabhbb <sadiyabhbb@proton.me>
2025-07-08 17:41:49 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+require('dotenv').config();
+const TelegramBot = require('node-telegram-bot-api');
+const fetchOtp = require('./checkOtp');
+const numbers = require('./numbers.json');
 
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+const sentOtps = {}; // OTP prevent duplicate send
+
+async function checkAllNumbers() {
+  for (const number of numbers) {
+    const otpData = await fetchOtp(number);
+
+    if (otpData && otpData.otp && sentOtps[number] !== otpData.otp) {
+      const msg = `🔔 *𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 OTP Received Successfully*\n\n` +
+                  `🕒 *Time:* ${otpData.time}\n` +
+                  `⚙️ *Service:* ${otpData.service}\n` +
+                  `📞 *Number:* ${otpData.number}\n` +
+                  `🔑 *Your OTP:* \`${otpData.otp}\`\n\n` +
+                  `💌 *Full Message:* ${otpData.message}\n\n` +
+                  `📖 حَسْبُنَا اللَّهُ وَنِعْمَ الْوَكِيلُ — *Allah is sufficient for us, and He is the best disposer.* (3:173)\n\n` +
+                  `🚀 *Be Active  New OTP Coming*`;
+
+      bot.sendMessage(process.env.GROUP_ID, msg, { parse_mode: 'Markdown' });
+      sentOtps[number] = otpData.otp;
+    }
+  }
+}
+
+// ⏱️ Check every 10 seconds
+setInterval(checkAllNumbers, 10000);
+
+console.log('✅ IVASMS OTP Bot running...');
